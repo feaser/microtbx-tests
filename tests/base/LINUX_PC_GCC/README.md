a1ccb55fdf0211f01c1eeb2701e957a0bb309e04
Frank Voorburg <voorburg@feaser.com>
2022-06-06 14:36:23 +0200

    Added the test program for running the MicroTBX unit tests.

---
+++ tests/base/LINUX_PC_GCC/README.md
@@ -0,0 +1,47 @@
+This program performs unit tests on the MicroTBX software components. It is targeted towards a GNU/Linux based PC and is configured for building with the GNU GCC toolchain.
+
+# Prerequisites
+
+The test program relies on [CMake](https://cmake.org/) to automatically detect the build tools on your system and to generate the build environment for it. On a Debian/Ubuntu based system, the following command installs the build dependencies:
+
+```
+sudo apt-get install build-essential cmake
+```
+
+# Getting the code
+
+The MicroTBX software component needs to be manually obtained:
+
+```
+cd third_party
+git clone https://github.com/feaser/microtbx.git microtbx
+git submodule update --init
+```
+
+Alternatively, you can clone and obtain the submodules in one go by using the `--recursive` argument. Example:
+
+```
+cd third_party
+git --recursive https://github.com/feaser/microtbx.git microtbx
+```
+
+# Building
+
+Using the terminal, set the working directory to the `./build` directory. Next, type the following two commands to detect and generate the Unix Makefiles based build environment and then build the test program:
+
+```
+cmake ..
+make
+```
+
+This creates the `microtbx_tests` executable in the `./build` directory. 
+
+# Running
+
+After building the test program, you can perform the unit tests by starting the `microtbx_tests` executable, located in the `./build` directory:
+
+```
+./microtbx_tests
+```
+
+The test program produces text on the standard output to inform you about its progress, including a final summary about the passed/failed tests.
---
+++ tests/base/LINUX_PC_GCC/main.c
@@ -0,0 +1,60 @@
+/************************************************************************************//**
+* \file         tests/base/LINUX_PC_GCC/main.c
+* \brief        Test program application source file.
+* \internal
+*----------------------------------------------------------------------------------------
+*                          C O P Y R I G H T
+*----------------------------------------------------------------------------------------
+*   Copyright (c) 2022 by Feaser     www.feaser.com     All rights reserved
+*
+*----------------------------------------------------------------------------------------
+*                            L I C E N S E
+*----------------------------------------------------------------------------------------
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* \endinternal
+****************************************************************************************/
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
+#include "unittests.h"                           /* Unit tests header                  */
+
+
+/************************************************************************************//**
+** \brief     This is the program entry point.
+** \param     argc Number of program arguments.
+** \param     argv Array with program arguments.
+** \return    Program exit code.
+**
+****************************************************************************************/
+int main(int argc, char *argv[])
+{
+  int result;
+
+  /* Initialize the unit test environment. */
+  initializeTests();
+  /* Run the unit tests. */
+  result = runTests();
+  /* Set the program exit code. */
+  return result;
+} /*** end of main ***/
+
+
+/*********************************** end of main.c *************************************/
