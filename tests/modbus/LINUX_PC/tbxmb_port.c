2ad3fa13115077812f28e1ac2bd70359d109a4d6
Frank Voorburg <voorburg@feaser.com>
2025-06-13 13:19:43 +0000

    Implemented port that crosslinks simulated serial ports.

--- tests/modbus/LINUX_PC/tbxmb_port.c
+++ tests/modbus/LINUX_PC/tbxmb_port.c
@@ -37,29 +37,9 @@
 /****************************************************************************************
 * Include files
 ****************************************************************************************/
+#include <sys/time.h>                            /* Time definitions                   */
 #include "microtbx.h"                            /* MicroTBX library                   */
 #include "microtbxmodbus.h"                      /* MicroTBX-Modbus library            */
-
-
-/****************************************************************************************
-* Function prototypes
-****************************************************************************************/
-void TbxMbPortUartTxInterrupt(tTbxMbUartPort port);
-void TbxMbPortUartRxInterrupt(tTbxMbUartPort port);
-
-
-/****************************************************************************************
-* Local data declarations
-****************************************************************************************/
-/** \brief Variable that groups together information to control a serial transmission
- *         for each serial port.
- */
-static volatile struct
-{
-  uint8_t  const * data;                 /**< Pointer of the transmit data byte array. */
-  uint16_t         nextIdx;              /**< Index of the next byte to transmit.      */
-  uint16_t         totalLen;             /**< Total number of bytes to transmit.       */
-} transmitInfo[TBX_MB_UART_NUM_PORT];
 
 
 /************************************************************************************//**
@@ -79,23 +59,16 @@ void TbxMbPortUartInit(tTbxMbUartPort     port,
                        tTbxMbUartStopbits stopbits,
                        tTbxMbUartParity   parity)
 {
-  TBX_UNUSED_ARG(port);
   TBX_UNUSED_ARG(baudrate);
   TBX_UNUSED_ARG(databits);
   TBX_UNUSED_ARG(stopbits);
   TBX_UNUSED_ARG(parity);
 
-  /* TODO ##Port 
-   * 
-   * Perform the following steps to initialize the UART channel:
-   *   - Enable the clock of the UART peripheral.
-   *   - Configure the UART Rx and Tx GPIO pins for UART communication.
-   *   - Switch the RS485 transceiver to reception mode (DE/NRE pins), if used.
-   *   - Configure the baudrate, number of databits, number of stopbits, and parity mode.
-   *   - Enable the UART transmitter and receiver.
-   *   - Enable the receive data register full (RXNE) interrupt.
+  /* This stub supports two simulated and cross-linked UART ports. As such this function
+   * does not need to actually initialize any hardware. Just check that specified port
+   * is within range.
    */
-
+  TBX_ASSERT((port == TBX_MB_UART_PORT1) || (port == TBX_MB_UART_PORT2));
 } /*** end of TbxMbPortUartInit ***/
 
 
@@ -118,24 +91,25 @@ uint8_t TbxMbPortUartTransmit(tTbxMbUartPort         port,
                               uint8_t        const * data, 
                               uint16_t               len)
 {
-  uint8_t result = TBX_OK;
+  uint8_t        result = TBX_ERROR;
+  tTbxMbUartPort rxPort;
 
-  /* Prepare the transmit information taking into account that this function will already
-   * start the transmission of the first byte.
-   */
-  transmitInfo[port].data = data;
-  transmitInfo[port].totalLen = len;
-  transmitInfo[port].nextIdx = 1U;
-
-  /* TODO ##Port 
-   * 
-   * - Switch the RS485 transceiver to transmission mode (DE/NRE pins), if used.
-   * - Write the first byte (transmitInfo[port].data[0]) to the UART transmit data
-   *   register.
-   * - Enable the transmit complete (TC) interrupt if the total length is just one
-   *   byte (len == 1), otherwise enable the transmit data register empty (TXE)
-   *   interrupt.
-   */
+  /* Verify parameters. */
+  TBX_ASSERT(((port == TBX_MB_UART_PORT1) || (port == TBX_MB_UART_PORT2)) &&
+             (data != NULL) && (len > 0U));
+  /* Only continue with valid parameters. */             
+  if (((port == TBX_MB_UART_PORT1) || (port == TBX_MB_UART_PORT2)) &&
+      (data != NULL) && (len > 0U))
+  {
+    /* Determine the serial port that should receive this data. */
+    rxPort = (port == TBX_MB_UART_PORT1) ? TBX_MB_UART_PORT2 : TBX_MB_UART_PORT1;
+    /* Inform the Modbus UART module about the newly received data byte. */
+    TbxMbUartDataReceived(rxPort, data, len);
+    /* Inform the Modbus UART module about the transmission completed event. */
+    TbxMbUartTransmitComplete(port);
+    /* Update the result to success. */
+    result = TBX_OK;
+  }
 
   /* Give the result back to the caller. */
   return result;
@@ -169,112 +143,20 @@ uint8_t TbxMbPortUartTransmit(tTbxMbUartPort         port,
 ****************************************************************************************/
 uint16_t TbxMbPortTimerCount(void)
 {
-  /* TODO ##Port 
-   * 
-   * Read out the current value of the timer's free running counter and return it.
-   * 
-   * This assumes you already initialized a timer, during application initialization, to
-   * have its free running counter counting upwards at 20 kHz. With other words, each
-   * count of the free running counter equals 50 microseconds.
-   * 
-   * Theoretically you could also use a timer to generate an interrupt every 50 us. The 
-   * interrupt service routine then increments a 16-bit unsigned integer counter. This
-   * function would then return this counter value. However, this interrupt driven timer
-   * approach would cause a high interrupt load. It is therefore better from a run-time
-   * performance perspective to simply  configure your timer to increment its free
-   * running counter every 50 microseconds.
-   */
+  uint16_t result = 0U;
+  struct   timeval tv;
 
-  return 0U;
+  if (gettimeofday(&tv, NULL) == 0)
+  {
+    /* Get the system time in microseconds and divice by 50 to get a timer counter that
+     * increments once every 50us. 
+     */
+    result = (uint16_t)(((tv.tv_sec * 1000000UL) + tv.tv_usec) / 50U);
+  }
+
+  /* Give the result back to the caller. */
+  return result;
 } /*** end of TbxMbPortTimerCount ***/
-
-
-/****************************************************************************************
-*            I N T E R R U P T   S E R V I C E   R O U T I N E S
-****************************************************************************************/
-/************************************************************************************//**
-** \brief     UART transmit complete / data register empty interrupt handler. Should be
-**            called from your UART interrupt handler for the specified serial port.
-** \param     port The serial port that generated the interrupt.
-**
-****************************************************************************************/
-void TbxMbPortUartTxInterrupt(tTbxMbUartPort port)
-{
-  /* Still data left to send? */
-  if (transmitInfo[port].nextIdx < transmitInfo[port].totalLen)
-  {
-    /* TODO ##Port 
-     * 
-     * - Write the next byte (transmitInfo.data[transmitInfo.nextIdx]) to the UART
-     *   transmit data register.
-     */
-
-    /* Currently transmitting the last byte of the entire transfer? */
-    if (transmitInfo[port].nextIdx == (transmitInfo[port].totalLen - 1U))
-    {
-      /* TODO ##Port 
-       * 
-       * - Disable the transmit data register empty (TXE) interrupt.
-       * - Enable the transmit complete (TC) interrupt.
-       */
-
-    }
-    /* Update the indexer to point to the next byte to transmit. */
-    transmitInfo[port].nextIdx++;
-  }
-  /* No more data left to send. */
-  else
-  {
-    /* TODO ##Port 
-     * 
-     * - Disable the transmit complete (TC) interrupt.
-     * - Switch the RS485 transceiver to reception mode (DE/NRE pins), if used.
-     */
-
-    /* Inform the Modbus UART module about the transmission completed event. */
-    TbxMbUartTransmitComplete(port);
-  }
-} /*** end of TbxMbPortUartTxInterrupt ***/
-
-
-/************************************************************************************//**
-** \brief     UART reception data register full interrupt handler. Should be called from
-**            your UART interrupt handler for the specified serial port.
-** \param     port The serial port that generated the interrupt.
-**
-****************************************************************************************/
-void TbxMbPortUartRxInterrupt(tTbxMbUartPort port)
-{
-  uint8_t errorDetected = TBX_FALSE;
-  uint8_t rxByte = 0U;
-
-  /* TODO ##Port 
-   * 
-   * - If a parity, framing or noise error was detected during the data reception, set
-   *   errorDetected = TBX_TRUE. 
-   *   - Note that there is no need to set this error flag in case of a reception
-   *     overrun.
-   * - Clear all reception related error flags: parity, framing, noise and also 
-   *   reception overrun.
-   * - Read the newly received data from the UART reception data register and store it in
-   *   rxByte.
-   * - Clear the reception data register empty flag, if the UART peripheral does not 
-   *   automatically do this after a read from the UART reception data register.
-   */
-
-  /* In case of a parity, framing or noise error, ignore the received data because it
-   * will be invalid. Nothing that can be done to save or correct the data. It is
-   * assumed that a higher module will be able to detect that data went missing. For
-   * example due to a data stream being shorter than expected or an incorrect checksum.
-   * In case of an overrun error, the data is still valid. Only the data in the
-   * reception shift register gets lost.
-   */
-  if (errorDetected == TBX_FALSE)
-  {
-    /* Inform the Modbus UART module about the newly received data byte. */
-    TbxMbUartDataReceived(port, &rxByte, 1U);
-  }
-} /*** end of TbxMbPortUartRxInterrupt ***/
 
 
 /*********************************** end of tbxmb_port.c *******************************/
